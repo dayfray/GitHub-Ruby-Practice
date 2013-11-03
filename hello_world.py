470eef56c17e7d6df0c3629ced11b8717cd8f3bc
DayFray <connacht@cyptolab.net>
2013-11-03 15:42:56 -0500

    I made it so fancy and with a comment, now

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,14 @@
-print "Hello World!"
-def add1(num):
-	result = 1 + int(num)
-	return result
+
+
+def what_name(name):
+
+	if len(name) > 0:
+		return name
+
+	else:
+		name = "world"
+		return name
+
+	print "Hello " + name + "!"
+
+print "Hello " + what_name(raw_input("Hello, and what is your name? "))
