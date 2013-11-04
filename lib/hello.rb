1ffdfadddbcaf747bd9a33be99fdcf1f23126877
DayFray <connacht@cyptolab.net>
2013-11-03 20:01:13 -0500

    Edited the hello.rb file

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,8 @@
-# this is a hello world file for ruby
+$ git cat-file -p c45f26b
+# Default is World
+# Author: Day Fray (theMan@manmanguyman.com)
 
-puts "Hello world"
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+
